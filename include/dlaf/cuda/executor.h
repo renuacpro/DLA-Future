e8a97d96f63aead395ea9a789c8559256ea7c3e2
Teodor Nikolov <teodor.nikolov22@gmail.com>
2020-05-24 21:50:58 +0200

    WIP Implement a CUBLAS pool
    - Improve some of the comments

--- include/dlaf/cuda/executor.h
+++ include/dlaf/cuda/executor.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <c++/9.3.0/x86_64-pc-linux-gnu/bits/c++config.h>
+#include <cstddef>
 #include <utility>
+#include <vector>
 
 #ifdef DLAF_WITH_CUDA
 #include <cublas_v2.h>
@@ -13,26 +16,30 @@
 #include <hpx/parallel/executors/execution_fwd.hpp>
 #include <hpx/util/yield_while.hpp>
 
+#include "dlaf/common/assert.h"
 #include "dlaf/cuda/error.h"
 
 namespace dlaf {
 
-// - A kernel launch will fail if it is issued to a stream that is not associated to the current
-//   device. [1]
 namespace internal {
 
 // Call the CUBLAS function and schedule an event after it to query when it's done.
+//
+// The executor calls `setCudaDevice()` multiple times on each thread, any previous assignment of CUDA
+// devices to threads is not preserved.
+//
+// Note: It is not recommended that multiple thread share the same CUBLAS handle because extreme care
+//       needs to be taken when changing or destroying the handle.
 template <typename F, typename... Ts>
 void call_cublas(int device, cublasHandle_t handle, F&& f, Ts&&... ts) noexcept {
   // Set the device corresponding to the CUBLAS handle.
   //
-  // The CUBLAS library context is tied to the current CUDA device [2]. A previous task scheduled on the
+  // The CUBLAS library context is tied to the current CUDA device [1]. A previous task scheduled on the
   // same thread may have set a different device, this makes sure the correct device is used. The
-  // function is considered very low overhead call [3].
+  // function is considered very low overhead call [2].
   //
-  // [1]: https://docs.nvidia.com/cuda/cuda-c-programming-guide/index.html#stream-and-event-behavior
-  // [2]: https://docs.nvidia.com/cuda/cublas/index.html#cublascreate
-  // [3]: CUDA Runtime API, section 5.1 Device Management
+  // [1]: https://docs.nvidia.com/cuda/cublas/index.html#cublascreate
+  // [2]: CUDA Runtime API, section 5.1 Device Management
   DLAF_CUDA_CALL(cudaSetDevice(device));
 
   // Use an event to query the CUBLAS kernel for completion. Timing is disabled for performance. [1]
@@ -53,6 +60,7 @@ void call_cublas(int device, cublasHandle_t handle, F&& f, Ts&&... ts) noexcept 
   {
     static hpx::lcos::local::mutex mt;
     std::lock_guard<hpx::lcos::local::mutex> lk(mt);
+    // TODO: set pointer mode?
     DLAF_CUBLAS_CALL(f(handle, std::forward<Ts>(ts)...));
     DLAF_CUDA_CALL(cudaEventRecord(event, stream));
   }
@@ -81,80 +89,87 @@ void call_cublas(int device, cublasHandle_t handle, F&& f, Ts&&... ts) noexcept 
 
 }
 
-// TODO: Do I need copy/move constructors?
-// TODO: use all GPUs by default but allow to specify less gpus
-// TODO: set pointer mode?
-// TODO: A new cublas context needs to be created if the device is reset
-// TODO: there needs to be a mutex between to ensure nothing is scheduled in-between
-// TODO: Spawn a task here !!
-// TODO: Make sure the stream is associated with the device
-// TODO: set DLAF_CUBLAS_CALL(cublasSetStream(handle_, stream)); when the CUBLAS handle is created
-// TODO: CUDA_LAUNCH_BLOCKING environment variable used for profiling?
-// TODO: implicit synchronizations ?
-// TODO: CUDA_API_PER_THREAD_DEFAULT_STREAM ??
-// TODO: consider using `cudaStreamCreateWithFlags()` with `cudaStreamNonBlocking` to get concurrency
-// from libraries to which you don't have full control.
-
-// Note: Streams are thread safe.
-//
-// https://github.com/pytorch/pytorch/blob/master/c10/cuda/CUDAStream.h
-
-// Maximum number of concurrent kernel execution :
-// https://docs.nvidia.com/cuda/cuda-c-programming-guide/index.html#features-and-technical-specifications__technical-specifications-per-compute-capability
-// Most have at least 16
-
-// TODO: It is advisable to minimize calls to `cublasCreate()` and `cublasDestroy()`. Note that
-// `cublasDeviceSynchronize()` is called when `cublasDestroy()` is called
-//
-// For multi-threaded applications that use the same device from different threads, the recommended
-// programming model is to create one CUBLAS handle per thread and use that CUBLAS handle for the entire
-// life of the thread.
-
-// Streams are a FIFO structure
-
-// https://docs.nvidia.com/cuda/cuda-c-programming-guide/index.html#implicit-synchronization
-
-// ** On the cost of stream creation and destruction
-//
 // For static frameworks the recommended best practice is to create all the needed streams upfront and
 // destroy them after the work is done. This pattern is not immediately applicable to PyTorch, but a per
 // device stream pool would achieve a similar effect. In particular, it would:
 //
 // https://github.com/pytorch/pytorch/issues/9646
 // https://github.com/rapidsai/rmm/issues/352
-
-// Alternative : For code that is compiled using the --default-stream per-thread compilation flag (or
-// that defines the CUDA_API_PER_THREAD_DEFAULT_STREAM macro before including CUDA headers (cuda.h and
-// cuda_runtime.h)), the default stream is a regular stream and each host thread has its own default
-// stream.
-
-// According to
-// https://stackoverflow.com/questions/3565793/is-there-a-maximum-number-of-streams-in-cuda
-// the maximum number of concurrent kernels executing on the Fermi GPU's is 16.
-
-// Register this thread with HPX, this should be done once for each external OS-thread intended to invoke
-// HPX functionality. Calling this function more than once on the same thread will report an error.
+// TODO: Note: Streams are thread safe.
 //
-// Unregister the thread from HPX, this should be done once in
-// the end before the external thread exists.
-// hpx::error_code ec(hpx::lightweight);  // ignore errors
-// hpx::register_thread(this_->rt_, "cuda", ec);
-// this_->set_data(hpx::util::unused);
-// hpx::unregister_thread(this_->rt_);
-
-// Enqueue a host function call in a stream. The function will be called after currently enqueued
-// work and will block work added after it.
+// https://github.com/pytorch/pytorch/blob/master/c10/cuda/CUDAStream.h
+// Create a pool for cublas_handles associated to different devices and streams.
 //
-// Note: CudaStreamAddCallback() was deprecated in CUDA 10
+// TODO: spawn a separate thread for each device.
 //
-// DLAF_CUDA_CALL(cudaLaunchHostFunc(stream, stream_callback, this));
+// TODO: clarify implicit synchronizations ?
+/// TODO: clarify Streams are a FIFO structure [1].
+//
+// [1]: CUDA Programming Guide, 3.2.5.5.4. Implicit Synchronization
+//
+// For multi-threaded applications that use the same device from different threads, the recommended
+// programming model is to create one CUBLAS handle per thread and use that CUBLAS handle for the entire
+// life of the thread. [1]
+//
+// [1]: cuBLAS Guide, 2.4 cuBLAS Helper Function Reference, cublasCreate()
+//
+class cublas_pool {
+  // The number of streams spawned per device depends on the supported maximum number of concurrent
+  // kernels that can execute in parallel on that device [1]. The number varies between different GPU
+  // models, most support at least 16. The current number is chosen to be more conservative but can be
+  // easily revised.
+  //
+  // [1]: CUDA Programming Guide, Table 15. Technical Specifications per Compute Capability
+  static constexpr std::size_t num_streams_per_device = 5;
+  std::vector<cublasHandle_t> handles_arr;
 
-// It is not recommended that multiple thread share the same CUBLAS handle
-// because extreme care needs to be taken when changing or destroying the
-// handle.
+public:
+  // This constructor can be used to specify the devices to which CUBLAS handles and streams should be
+  // initialized. The constructor is useful if only a subset of the devices are used for CUBLAS calls.
+  cublas_pool(std::vector<int> const& devices) noexcept {
+    int num_devices = numDevices();
+    handles.reserve(num_streams_per_device * devices.size());
 
-// The executor calls `setCudaDevice()` multiple times on each thread, any previous assignment of CUDA
-// devices to threads is not preserved.
+    for (device : devices) {
+      for (int i_stream = 0; i_stream < num_streams_per_device; ++i_stream) {
+        cudaSetDevice(device);
+        // - A kernel launch will fail if it is issued to a stream that is not associated to the current
+        //   device. [1]
+        //
+        // [1]: https://docs.nvidia.com/cuda/cuda-c-programming-guide/index.html#stream-and-event-behavior
+        cudaStream_t stream;
+        cudaStreamCreateWithFlags(&stream, cudaStreamNonBlocking);
+        cublasHandle_t handle;
+        cublasCreate(&handle);
+        cublasSetStream(handle, stream);
+        handles.push_back(handle);
+      }
+    }
+  }
+
+  inline void printDeviceInfo(int device) const noexcept {
+    cudaDeviceProp deviceProp;
+    cudaGetDeviceProperties(&deviceProp, device);
+    printf("Device %d has compute capability %d.%d.\n", device, deviceProp.major, deviceProp.minor);
+  }
+
+  inline int numDevices() const noexcept {
+    int num_devices;
+    cudaGetDeviceCount(&num_devices);
+    return num_devices;
+  }
+
+  ~cublas_pool() noexcept {
+    for (cublasHandle_t handle : handles_arr) {
+      // This implicitly calls `cublasDeviceSynchronize()` [1].
+      //
+      // [1]: cuBLAS, section 2.4 cuBLAS Helper Function Reference
+      cublasDestroy(handle);
+    }
+  }
+};
+
+// An executor for CUBLAS functions.
 struct cublas_executor {
   // Associate the parallel_execution_tag executor tag type as a default with this executor.
   using execution_category = hpx::parallel::execution::parallel_execution_tag;
